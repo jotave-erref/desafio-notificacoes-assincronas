35c515f70629f5e984701fbce7e97b01b85c2785
jotave-erref <jvrferreira96@gmail.com>
2025-11-19 20:43:03 -0300

    chore: consolidate and improve global .gitignore file

---
+++ alert-processor/src/test/java/com/jotave/alert_processor/AlertProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotave.alert_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
