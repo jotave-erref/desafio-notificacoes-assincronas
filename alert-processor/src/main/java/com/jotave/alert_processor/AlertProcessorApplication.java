d252f5b540801948a76a18127448dadcd9b657c0
jotave-erref <jvrferreira96@gmail.com>
2025-11-19 20:21:29 -0300

    feat(processor): scaffold alert-processor microservice structure

---
+++ alert-processor/src/main/java/com/jotave/alert_processor/AlertProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.jotave.alert_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertProcessorApplication.class, args);
+	}
+
+}
